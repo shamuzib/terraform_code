6d505bf503bff933c7ab8d859352d4b5b4c8d1fc
shamuzib <shaiksami179@gmail.com>
2022-10-03 13:27:27 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "prod-ec2-web" {
-  ami           = var.ami_type
-  instance_type = var.ec2_type
-  associate_public_ip_address = true
-  availability_zone = var.availability_zone
-  security_groups = xxxxxxxxxxxxxxxxxxxxxxxxxxxxx # Create and setup a SG for Webtier EC2
-  tags          = local.common_tags
-}
+# resource "aws_instance" "prod-ec2-web" {
+#   ami           = var.ami_type
+#   instance_type = var.ec2_type
+#   associate_public_ip_address = true
+#   availability_zone = var.availability_zone
+#   security_groups = xxxxxxxxxxxxxxxxxxxxxxxxxxxxx # Create and setup a SG for Webtier EC2
+#   tags          = local.common_tags
+# }
