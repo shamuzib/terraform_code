6ecb2822ec4e74fa05e985cacb3cd53896e9b95b
shamuzib <shaiksami179@gmail.com>
2023-02-27 23:27:11 +0000

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -16,3 +16,29 @@ def check_vault_sealed():
         return "Vault is sealed, please unseal the Vault"
     else:
         return "Vault is unsealed"
+
+    
+import subprocess
+
+def check_vault(vault_url, vault_token, secret_path):
+    # Check if Vault server is sealed
+    sealed = subprocess.run(['vault', 'status', '-format=json'], capture_output=True)
+    if sealed.returncode != 0:
+        raise RuntimeError('Error checking Vault status')
+    
+    sealed_json = json.loads(sealed.stdout)
+    if sealed_json['sealed']:
+        return "Vault is sealed, please unseal the Vault"
+    
+    # Check if secret path exists
+    path_exists = subprocess.run(['vault', 'kv', 'get', '-format=json', secret_path], 
+                                 env={'VAULT_ADDR': vault_url, 'VAULT_TOKEN': vault_token},
+                                 capture_output=True)
+    
+    if path_exists.returncode == 0:
+        # Path exists, read and display output
+        secret_value = json.loads(path_exists.stdout)['data']['data']
+        return f"Secret value for path {secret_path}: {secret_value}"
+    else:
+        # Path does not exist
+        return f"Path {secret_path} does not exist"
